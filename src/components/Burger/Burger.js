2c1fbafe1cd7c30487f3b28c68487e04eb7cee5a
WuZhengda <dalianmuch@outlook.com>
2019-03-20 17:00:56 -0400

    Add files via upload

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import Burgerin from "./Burgerin/Burgerin";
+
+function burger(props) {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <Burgerin key={igKey + i} type={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p> You could add something here! </p>;
+    }
+    return (
+        <div className={classes.Burger}>
+            <Burgerin type="bread-top" />
+            {transformedIngredients}
+            <Burgerin type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burger;
